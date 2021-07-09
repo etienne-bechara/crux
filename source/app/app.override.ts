9cc8661f7863bdee9b4a54e465938d602a9fcc02
Etienne Bechara <bechara.etienne@gmail.com>
2021-07-09 10:59:56 -0300

    feat: add overrides of all used deps

--- source/app/app.override.ts
+++ source/app/app.override.ts
@@ -1,4 +1,6 @@
-import { HttpAdapterHost } from '@nestjs/common';
+import { Head, HttpAdapterHost, ValidationError } from '@nestjs/common';
+import { Type } from 'class-transformer';
+import { isEmpty, max, min, validate } from 'class-validator';
 
 import { HttpModuleOptions } from '../http/http.interface';
 import { HttpModule } from '../http/http.module';
@@ -8,5 +10,12 @@ import { LoggerService } from '../logger/logger.service';
 export * from '@nestjs/common';
 export * from '@nestjs/core';
 export * from '@nestjs/platform-express';
+export * from 'class-transformer';
+export * from 'class-validator';
+export * from 'rxjs';
+export * from 'uuid';
 
-export { HttpAdapterHost, HttpModule, HttpModuleOptions, HttpService, LoggerService };
+export {
+  Head, HttpAdapterHost, HttpModule, HttpModuleOptions, HttpService, isEmpty,
+  LoggerService, max, min, Type, validate, ValidationError,
+};
