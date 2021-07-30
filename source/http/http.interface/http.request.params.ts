9fb8cd951d709cdf9c78484a7e5fd49d7df3b923
Etienne Bechara <bechara.etienne@gmail.com>
2021-07-30 20:49:49 -0300

    fix: search params type

--- source/http/http.interface/http.request.params.ts
+++ source/http/http.interface/http.request.params.ts
@@ -3,4 +3,5 @@ import { OptionsOfTextResponseBody } from 'got';
 export interface HttpRequestParams extends OptionsOfTextResponseBody {
   /** Object containing replacement string for path variables. */
   replacements?: Record<string, string>;
+  searchParams?: Record<string, any>;
 }
