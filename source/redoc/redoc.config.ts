f1847a32a274fb49c3528b258b1720c9e1100f7c
Etienne Bechara <bechara.etienne@gmail.com>
2022-03-02 21:10:31 -0300

    fix: typo

--- source/redoc/redoc.config.ts
+++ source/redoc/redoc.config.ts
@@ -12,7 +12,7 @@ export class RedocConfig {
       url: 'https://www.openapis.org/wp-content/uploads/sites/3/2016/10/OpenAPI_Pantone.png',
     },
     description: 'This API documentation is automatically generated based on `@nestjs/swagger` decorators.\n\n'
-      + 'For further instructions on how to annotated your models and endpoints check '
+      + 'For further instructions on how to annotate your models and endpoints check '
       + '[NestJS - OpenAPI Introduction](https://docs.nestjs.com/openapi/introduction).\n\n'
       + 'To customize logo, title, description and other layout options, add the `redoc` property '
       + 'during application initialization:\n\n```\nvoid AppModule.boot({\n  redoc: { }\n});\n```',
