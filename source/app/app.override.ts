6584c7f91561a0a41deef1a7f0ee42224d6c5d88
Etienne Bechara <bechara.etienne@gmail.com>
2022-03-18 10:26:42 -0300

    fix: swagger overrides

--- source/app/app.override.ts
+++ source/app/app.override.ts
@@ -9,6 +9,7 @@ export * from 'prom-client';
 export * from 'rxjs';
 
 export { ValidationError, HttpAdapterHost } from '@nestjs/common';
+export { IntersectionType, OmitType, PartialType, PickType } from '@nestjs/swagger';
 export { v1 as uuidV1, v3 as uuidV3, v4 as uuidV4, v5 as uuidV5 } from 'uuid';
 
 export { Type } from 'class-transformer';
