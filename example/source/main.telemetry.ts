318e63fe2668b5c5e4dc2fcd3b89e9dafc6c32ba
Etienne Bechara <bechara.etienne@gmail.com>
2022-04-11 13:03:25 -0300

    fix: split dev script into telemetry

---
+++ example/source/main.telemetry.ts
@@ -0,0 +1,30 @@
+import { AppModule } from '../../source/app/app.module';
+import { RandomModule } from './random/random.module';
+import { UserModule } from './user/user.module';
+import { ZipModule } from './zip/zip.module';
+
+/**
+ * Run with `pnpm telemetry`.
+ * Boots application with full telemetry functionalities.
+ */
+void AppModule.boot({
+  job: 'nestjs-example',
+  disableScan: true,
+  imports: [
+    RandomModule,
+    UserModule,
+    ZipModule,
+  ],
+  loki: {
+    url: 'http://localhost:3100',
+    pushInterval: 5000,
+  },
+  metrics: {
+    url: 'http://127.0.0.1:9091',
+    pushInterval: 5000,
+  },
+  traces: {
+    url: 'http://127.0.0.1:55681',
+    pushInterval: 5000,
+  },
+});
--- example/source/main.ts
+++ example/source/main.ts
@@ -3,24 +3,21 @@ import { RandomModule } from './random/random.module';
 import { UserModule } from './user/user.module';
 import { ZipModule } from './zip/zip.module';
 
+/**
+ * Run with `pnpm dev`.
+ * Boots application with basic functionalities.
+ *
+ * In a real scenario the `AppModule` would be imported from `@bechara/nestjs-core`
+ * and disabling scan as well as manually importing your modules unnecessary.
+ */
 void AppModule.boot({
-  job: 'nestjs-example',
+  disableTraces: true,
+
+  // Not required when running from another project
   disableScan: true,
   imports: [
     RandomModule,
     UserModule,
     ZipModule,
   ],
-  loki: {
-    url: 'http://localhost:3100',
-    pushInterval: 5000,
-  },
-  metrics: {
-    url: 'http://127.0.0.1:9091',
-    pushInterval: 5000,
-  },
-  traces: {
-    url: 'http://127.0.0.1:55681',
-    pushInterval: 5000,
-  },
 });
