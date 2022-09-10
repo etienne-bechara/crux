a18b716d5d207a525d34e8e737b194439cddb1e4
Etienne Bechara <bechara.etienne@gmail.com>
2022-09-10 16:16:45 -0300

    fix: @Span() for async

--- source/trace/trace.decorator.ts
+++ source/trace/trace.decorator.ts
@@ -13,23 +13,40 @@ export function Span(name?: string, options?: SpanOptions) {
   return (target: any, propertyKey: string, propertyDescriptor: PropertyDescriptor): void => {
     const spanName = name || `${target.constructor.name}.${propertyKey}()`;
     const sourceMethod = propertyDescriptor.value;
+    const isAsync = sourceMethod.constructor.name === 'AsyncFunction';
 
     const wrapperMethod = function PropertyDescriptor(...args: any[]): any {
-      return TraceService.startActiveSpan(spanName, options, async (span: Span) => {
-        try {
-          const result = await sourceMethod.apply(this, args);
-          span.setStatus({ code: SpanStatusCode.OK });
-          return result;
-        }
-        catch (e) {
-          span.recordException(e as Error);
-          span.setStatus({ code: SpanStatusCode.ERROR, message: e.message });
-          throw e;
-        }
-        finally {
-          span.end();
-        }
-      });
+      return isAsync
+        ? TraceService.startActiveSpan(spanName, options, async (span: Span) => {
+          try {
+            const result = await sourceMethod.apply(this, args);
+            span.setStatus({ code: SpanStatusCode.OK });
+            return result;
+          }
+          catch (e) {
+            span.recordException(e as Error);
+            span.setStatus({ code: SpanStatusCode.ERROR, message: e.message });
+            throw e;
+          }
+          finally {
+            span.end();
+          }
+        })
+        : TraceService.startActiveSpan(spanName, options, (span: Span) => {
+          try {
+            const result = sourceMethod.apply(this, args);
+            span.setStatus({ code: SpanStatusCode.OK });
+            return result;
+          }
+          catch (e) {
+            span.recordException(e as Error);
+            span.setStatus({ code: SpanStatusCode.ERROR, message: e.message });
+            throw e;
+          }
+          finally {
+            span.end();
+          }
+        });
     };
 
     propertyDescriptor.value = wrapperMethod;
