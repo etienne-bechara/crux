6bab95a065178cec3199809ad1190173f2ae6664
Etienne Bechara <bechara.etienne@gmail.com>
2024-10-23 11:29:44 -0300

    fix(orm): remove entity recursion removal due to performance issues

--- source/orm/orm.entity.ts
+++ source/orm/orm.entity.ts
@@ -3,64 +3,7 @@ import { v4 as uuidV4 } from 'uuid';
 
 import { OrmBigIntDto, OrmBigIntTimestampDto, OrmIntDto, OrmIntTimestampDto, OrmTimestampDto, OrmUuidDto, OrmUuidTimestampDto } from './orm.dto.out';
 
-export abstract class OrmBaseEntity extends BaseEntity<AnyEntity, 'id'> {
-
-  /**
-   * During serialization eliminate recursion to self entity.
-   * @param args
-   */
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  public toJSON(...args: any[]): any {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const serializedObject = super.toJSON(...args);
-    const primitiveClone: Record<string, unknown> = { };
-
-    for (const key in serializedObject) {
-      if ([ 'string', 'number', 'boolean' ].includes(typeof serializedObject[key])) {
-        primitiveClone[key] = serializedObject[key];
-      }
-    }
-
-    return this.deleteRecursion(serializedObject, primitiveClone);
-  }
-
-  /**
-   * Delete nested occurrences of current entity.
-   * @param obj
-   * @param parent
-   */
-  private deleteRecursion(obj: any, parent: Record<string, unknown>): any {
-    const parentKeys = Object.keys(parent).length;
-    if (!parentKeys) return obj;
-
-    for (const objKey in obj) {
-      const value = obj[objKey];
-
-      if (Array.isArray(value)) {
-        obj[objKey] = value.map((i) => this.deleteRecursion(i, parent));
-      }
-      else if (typeof value === 'object') {
-        let matchingKeys = 0;
-
-        for (const parentKey in parent) {
-          if (obj[objKey]?.[parentKey] === parent[parentKey]) {
-            matchingKeys++;
-          }
-          else {
-            break;
-          }
-        }
-
-        if (parentKeys === matchingKeys) {
-          delete obj[objKey];
-        }
-      }
-    }
-
-    return obj;
-  }
-
-}
+export abstract class OrmBaseEntity extends BaseEntity<AnyEntity, 'id'> { }
 
 export abstract class OrmIntEntity extends OrmBaseEntity implements OrmIntDto {
 
