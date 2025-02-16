933b0041dba65e3e6e0d569226182fab7104dbc2
Etienne Bechara <bechara.etienne@gmail.com>
2025-02-16 20:23:52 -0300

    fix(jest): test config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('jest').Config} */
+const config = {
+  rootDir: 'source',
+  verbose: true,
+  forceExit: true,
+  collectCoverage: true,
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  testRegex: '.spec.ts$',
+  testResultsProcessor: 'jest-sonar-reporter',
+  testTimeout: 30000,
+  transform: {
+    ts$: 'ts-jest'
+  },
+};
+
+module.exports = config;
